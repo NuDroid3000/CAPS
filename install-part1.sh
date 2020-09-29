b3cbef0c5b829d353a1ef084c810e337088ceb9e
Colean <11258171+Colean128@users.noreply.github.com>
2020-09-29 20:34:21 +0100

    Update install-part1.sh

--- install-part1.sh
+++ install-part1.sh
@@ -4,19 +4,17 @@
 INSTALLDRIVE=/dev/sda
 
 loadkeys uk
-echo Loaded UK keymap.
+echo Loaded UK keymap. && echo
 
 timedatectl set-ntp true
-echo System clock updated.
+echo System clock updated. && echo
 
 fdisk -l
-echo Before saying yes, if the drive you want to install to is not /dev/sda
-echo Open up the bash script and edit the INSTALLDRIVE variable.
+echo && echo Before saying yes, if the drive you want to install to is not /dev/sda
+echo Open up the bash script and edit the INSTALLDRIVE variable. && echo
 parted $INSTALLDRIVE mklabel msdos
 
-echo Formatted hard drive.
+echo Formatted hard drive. && echo
 echo Enter -1 for end.
 parted $INSTALLDRIVE mkpart primary ext4 512MiB
 echo Created partition.
-
-print fart
