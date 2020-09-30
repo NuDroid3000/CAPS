da05259385e1249a20f339188fc82cc491cab34a
Colean <11258171+Colean128@users.noreply.github.com>
2020-09-30 16:02:32 +0100

    Update kde-plasma-finalise.sh

--- kde-plasma-finalise.sh
+++ kde-plasma-finalise.sh
@@ -5,9 +5,6 @@
 sudo pacman -Syu xorg
 sudo pacman -Syu plasma
 sudo pacman -Syu kde-applications
-echo 'if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then' >> ~/.bash_profile
-echo '  exec startx' >> ~/.bash_profile
-echo 'fi' >> ~/.bash_profile
-echo 'export DESKTOP_SESSION=plasma' >> ~/.xinitrc
-echo 'exec startplasma-x11' >> ~/.xinitrc
-xinit
+sudo systemctl enable sddm
+sudo systemctl enable NetworkManager
+echo If you see this, run reboot now.
