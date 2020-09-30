4940cb8174a63b511d50eb062c81d41d31679556
Colean <11258171+Colean128@users.noreply.github.com>
2020-09-30 08:09:43 +0100

    Create kde-plasma-finalise.sh

---
+++ kde-plasma-finalise.sh
@@ -0,0 +1,11 @@
+# Colean arch provisioning finalise script for KDE Plasma
+# Requires Xorg installation.
+
+sudo pacman -Syu xorg
+sudo pacman -Syu plasma
+sudo pacman -Syu kde-applications
+echo if [[ ! $DISPLAY && $XDG_VTNR -eq 1 ]]; then >> ~/.bash_profile
+echo exec startx >> ~/.bash_profile
+echo fi >> ~/.bash_profile
+echo export DESKTOP_SESSION=plasma >> ~/.xinitrc
+echo exec startplasma-x11 >> ~/.xinitrc
