f0ad259bc6296a81b9daaedf01f579964acddedc
Colean <11258171+Colean128@users.noreply.github.com>
2020-10-01 20:25:12 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ MIRROR=https://raw.githubusercontent.com/Colean128/arch-provision/master
 #MIRROR=https://ap.colean.cc/m
 PART1OPTIONS=
 
-curl -L ${MIRROR}/install-part1-${PART1OPTIONS} >> install-part1.sh
+curl -L ${MIRROR}/install-part1-${PART1OPTIONS}.sh >> install-part1.sh
 rm -r run.sh
 echo Run install-part1.sh to begin!
