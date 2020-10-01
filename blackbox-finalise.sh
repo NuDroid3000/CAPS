0d4ec6bad51bcd6e5b30690ec71e2b9a3c55bb1a
Colean <11258171+Colean128@users.noreply.github.com>
2020-10-01 16:08:58 +0100

    Create blackbox-finalise.sh

---
+++ blackbox-finalise.sh
@@ -0,0 +1,8 @@
+# blackbox finalise script
+# Don't run as root.
+
+sudo pacman -Syu xorg xorg-xinit blackbox xterm
+echo 'if [[ "$(tty)" = "/dev/tty1" ]]; then' >> ~/.bash_profile
+echo '  exec startx' >> ~/.bash_profile
+echo 'fi' >> ~/.bash_profile
+echo You can now reboot the system into blackbox.
